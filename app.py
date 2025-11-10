638e6f8b7e082008d7d3b0a57016ed6b4f1ba0e7
Albert Chore <79362590+achore26@users.noreply.github.com>
2025-11-10 12:22:45 +0300

    Added Token authentication

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ from flask import Flask, request , jsonify
 import mysql.connector, os
 from dotenv import load_dotenv
 from flasgger import Swagger
+import jwt
+import datetime
 
 load_dotenv()
 
 app = Flask(__name__)
 swagger = Swagger(app)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 conn = mysql.connector.connect(
     host=os.getenv("DB_HOST"),
@@ -16,6 +19,21 @@ conn = mysql.connector.connect(
 )
 
 cursor = conn.cursor()
+
+def verify_token():
+    token = request.headers.get('Authorization')
+
+    if not token:
+        return jsonify({"Error" : "There is no token detected"}),403
+    
+    try:
+        decoded_token = jwt.decode(token, app.config['SECRET_KEY'], algorithms=["HS256"])
+        username = decoded_token['username']
+    except jwt.ExpiredSignatureError:
+        return jsonify({"message": 'Token expired!'}), 401
+    except jwt.InvalidTokenError:
+        return jsonify({"messsage" : "Invalid Token"}), 401
+
 
 @app.route('/register', methods=['POST','GET'])
 def register():
@@ -31,21 +49,24 @@ def register():
                 examples:
                     application/json: "User registered successfully"
     """
-    data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
-    email = data.get('email')
-    role = data.get('role')
+    if request.method == 'POST':
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
+        email = data.get('email')
+        role = data.get('role')
 
-   
-    add_user = "INSERT INTO user (username, email, password, role) VALUES (%s, %s, %s, %s)"
-    cursor.execute(add_user, (username, email, password, role))
-    conn.commit()
- 
-    return jsonify({'message': 'User registered successfully'}), 201
+    
+        add_user = "INSERT INTO user (username, email, password, role) VALUES (%s, %s, %s, %s)"
+        cursor.execute(add_user, (username, email, password, role))
+        conn.commit()
+    
+        return jsonify({'message': 'User registered successfully'}), 201
+    
+    return jsonify({"Message" : "This is the registration page."})
 
 
-@app.route('/login', methods=['POST'])
+@app.route('/login', methods=['POST','GET'])
 def login():
     """
         This is an example endpoint that logs in  a user.
@@ -63,19 +84,24 @@ def login():
                 examples:
                     application/json: "Invalid username or password"
     """
-    data = request.get_json()
-    username = data.get('username')
-    password = data.get('password')
+    if request.method == 'POST':
+        data = request.get_json()
+        username = data.get('username')
+        password = data.get('password')
 
-    cursor = conn.cursor()
-    check_user = "SELECT * FROM user WHERE username = %s AND password = %s"
-    cursor.execute(check_user, (username, password))
-    user = cursor.fetchone()
+        cursor = conn.cursor()
+        check_user = "SELECT * FROM user WHERE username = %s AND password = %s"
+        cursor.execute(check_user, (username, password))
+        user = cursor.fetchone()
 
-    if user:
-        return jsonify({'message': 'Accepted', 'role': user['role']}), 200
-    else:
-        return jsonify({'message': 'error'}), 401
+        if user:
+            Token = encoded_jwt = jwt.encode({"username": username, "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes =2)}, app.config['SECRET_KEY'], algorithm="HS256")
+
+            return jsonify({'message': 'Accepted', 'role': user['role']}), 200
+        else:
+            return jsonify({'message': 'error, this user may not exist'}), 401
+        
+    return jsonify({"Message" : "Hello, This is the login page"})
 
 @app.route('/<role>/events', methods=['GET'])
 def list_events(role):
@@ -92,7 +118,8 @@ def list_events(role):
                 examples:
                     application/json: "tennis event"
     """
-
+ 
+    verify_token()
     cursor = conn.cursor()
 
     if role == 'organizer':
@@ -108,7 +135,7 @@ def list_events(role):
         return jsonify({'available_events': events}), 200
 
 
-@app.route('/rsvp', methods=['POST'])
+@app.route('/rsvp', methods=['POST','GET'])
 def rsvp():
     """
         This is an example endpoint that rsvp's the attendee's.
@@ -122,21 +149,25 @@ def rsvp():
                 examples:
                     application/json: "attendance Confirmed"
     """
-    cursor = conn.cursor()
+    if request.method == 'POST':
+        verify_token()
+        cursor = conn.cursor()
 
-    data = request.get_json()
-    user_id = data.get('user_id')
-    event_id = data.get('event_id')
+        data = request.get_json()
+        user_id = data.get('user_id')
+        event_id = data.get('event_id')
 
-    rsvp = "INSERT INTO rsvp (user_id, event_id) VALUES (%s, %s)"
-    cursor.execute(rsvp, (user_id, event_id))
-    conn.commit()
+        rsvp = "INSERT INTO rsvp (user_id, event_id) VALUES (%s, %s)"
+        cursor.execute(rsvp, (user_id, event_id))
+        conn.commit()
 
-    return jsonify({'message': 'confirmed attendance'}), 201
+        return jsonify({'message': 'confirmed attendance'}), 201
+    
+    return jsonify({"Message" : "You have reached the rsvp's page "}),200
 
 
-    @app.route('/approve', methods=['POST'])
-    def approve():
+@app.route('/approve', methods=['POST','GET'])
+def approve():
         """
             This is an example endpoint that approves or rejects the rsvp's.
             ---
@@ -149,16 +180,22 @@ def rsvp():
                     examples:
                         application/json: "RSVP updated"
         """
-        cursor = conn.cursor()
-        data = request.get_json()
-        rsvp_id = data.get('rsvp_id')
-        status = data.get('status')
+        
+        if request.method == 'POST': 
+            verify_token()
 
-        update_status = "UPDATE rsvp SET status = %s WHERE id = %s"
-        cursor.execute(update_status, (status, rsvp_id))
-        conn.commit()
+            cursor = conn.cursor()
+            data = request.get_json()
+            rsvp_id = data.get('rsvp_id')
+            status = data.get('status')
 
-        return jsonify({'message': 'RSVP updated'}), 200
+            update_status = "UPDATE rsvp SET status = %s WHERE id = %s"
+            cursor.execute(update_status, (status, rsvp_id))
+            conn.commit()
+
+            return jsonify({'message': 'RSVP updated'}), 200
+        
+        return jsonify({"Message" : "This is the approved page"}),200
 
 if __name__ == '__main__':
     app.run (debug=True)
